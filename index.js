96eea8ed941beed8e713eb79efa5ee6246c06c0e
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-11-17 00:51:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much.";
+fetch('https://api.github.com/user/repos').
+then(res => res.json()).
+then(json => console.log(json))
+
+
+const token = 'ebc9f3377e3f49d92a343527542175fc114d2be9'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
